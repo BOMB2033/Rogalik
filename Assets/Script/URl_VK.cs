2cb471ae292abc822489231a4a9a5344f7900568
ArtMasterYukihiro <117318023+ArtMasterYukihiro@users.noreply.github.com>
2022-12-18 14:37:40 +0300

    ArtMaster

---
+++ Assets/Script/URl_VK.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URl_VK : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void silkka()
+    {
+		Application.OpenURL ("https://vk.com/theblindweavergames121");
+    }
+}
